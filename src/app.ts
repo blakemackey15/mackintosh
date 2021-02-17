37e9fedc2bbc4b9051bf210098223482e4f79da4
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-02-17 13:40:51 -0500

    updated server to send index.html

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile('index.html', {root: './views'});
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
