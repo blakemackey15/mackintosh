f5c3e8b36a6a79cbff384d63d5f4feb57fbfcd5e
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-02-19 15:14:21 -0500

    added files and grammar reference link to test tsc compilation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-import express from 'express';
-
-const app = express();
-const port = 3000;
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = express_1["default"]();
+var port = 3000;
 var bodyParser = require('body-parser');
-
-app.use(express.static('public'));
+app.use(express_1["default"].static('public'));
 app.use(bodyParser.json({ type: 'application/json' }));
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: '/views'});
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: './views' });
 });
-
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+app.listen(port, function () {
+    return console.log("server is listening on " + port);
 });
