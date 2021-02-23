d2190bb542574415dc29529585d60dd77e7da012
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-02-23 16:45:16 -0500

    added digit, symbol, and char regular expression

--- src/lexer/lex.ts
+++ src/lexer/lex.ts
@@ -3,11 +3,6 @@ import {token} from "./token";
 
 class lex {
     //regular expressions to represent our language's grammar. these expressions are declared in the rule order
-    private keyword = new RegExp('');
-    private id = new RegExp('');
-    private symbol = new RegExp('');
-    private digit = new RegExp('');
-    private braces = new RegExp('');
 
     constructor() {
 
--- src/lexer/token.ts
+++ src/lexer/token.ts
@@ -1,20 +1,25 @@
-//class that represents a single token in the lex token stream
+//Class that represents a single token in the lex token stream.
 
 /*
 Regular expressions must be made to test inputted strings.
 Tokens needed: 
-Keywords: print(), while, if, int, string, boolean, false, true
+Keywords: print(), while, if, int, string, boolean, false, true.
 Symbols: Comments, { }, ( ), ==, !=, =, $, ''
     If a character is in '', its a char/string. If it is not, it is a id.
 Characters: a, b, c, space, etc.
-Digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+Digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9.
 Identifiers: 
 */
 
 export class token {
 
     private tokenName : String;
+    //Represents integer digits 0-9.
     private digits = new RegExp('[0-9]');
+    //Regular expression catches all characters from any language using unicode including spaces.
+    private characters = new RegExp('[\p{L}\s]')
+    //Represents relevant symbols for the compiler.
+    private symbols = new RegExp('[-!$*()+={}\[\]"\/]')
 
     constructor(name: String) {
         this.tokenName = name;
