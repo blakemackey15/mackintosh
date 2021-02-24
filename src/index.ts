cfac25f20f2ce11374f633443baa8b772ef88576
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-02-24 17:04:15 -0500

    created global variables

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ References: Here is a list of the resources I referenced while developing this p
 https://www.debuggex.com/ - Useful tool I used to test my regular expressions for my tokens.
 */
 
+import {lex} from './lexer/lex';
+import {token} from './lexer/token';
 
-
-
+const input : HTMLFormElement = document.querySelector('#input');
+let _Lexer : lex;
+let tokenStream : Array<token>;
+let curToken : number;
+let errors : number;
