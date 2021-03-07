63944904f31aaeb36081e26656ea43118042d58b
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-03-07 15:04:56 -0500

    fixed tsc compile bug

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+/*
+References: Here is a list of the resources I referenced while developing this project.
+https://regex101.com/ - Useful tool I used to test my regular expressions for my tokens.
+*/
+
+// //Import Objects and css
+// import './public/css/mackComp';
+// const inputForm : HTMLFormElement = document.querySelector('#input');
+
+// inputForm.onsubmit = () => {
+//     const formData = new FormData(inputForm);
+//     console.log(formData)
+//     const text = formData.get('textInput') as string;
+//     console.log(text);
+//     return false;
+// };
+
+import {lex} from './lexer/lex';
+
+export class index {
+    private _lexer : lex;
+    public isCompiling : boolean = false;
+
+    constructor() {
+        this._lexer = new lex();
+        console.log('pee pee');
+        this.startCompile();
+    }
+
+    public startCompile() : boolean {
+        console.log('BEGINNING COMPILATION...');
+        let inputtedCode = this._lexer.testProgram();
+        this._lexer.lex();
+        return true;
+    }
+
+    public endCompule() : boolean {
+        return false;
+    }
+
+}
--- src/lexer/lex.js
+++ src/lexer/lex.js
@@ -1,55 +1,44 @@
-import {token} from "./token";
-
-
-export class lex {
-    private token : token;
-    private tokenBuffer : number;
-    private errorCount : number;
-    private tokenStream : Array<string>
-    private tokenFlag : boolean;
-    private tokens : Array<token>;
-
-    constructor() {
+"use strict";
+exports.__esModule = true;
+exports.lex = void 0;
+var token_1 = require("./token");
+var lex = /** @class */ (function () {
+    function lex() {
         this.tokenBuffer = 0;
         this.errorCount = 0;
-        this.token = new token;
+        this.token = new token_1.token;
         this.tokenFlag = false;
     }
-
-    public testProgram() : string {
-        let testProgram = '$';
+    lex.prototype.testProgram = function () {
+        var testProgram = '$';
         //Remove white spaces.
         testProgram.trim();
-
         //Push characters in string to token stream.
-        for(let i = 0; i < testProgram.length; i++) {
+        for (var i = 0; i < testProgram.length; i++) {
             this.tokenStream.push(testProgram.charAt(i));
             //Print characters in the token stream.
             console.log(testProgram.charAt(i));
         }
-
         return testProgram;
-    }
+    };
     //inputtedCode : string
-    public lex() { 
-        let code = this.testProgram();
-        
+    lex.prototype.lex = function () {
+        var code = this.testProgram();
         //Loop through the length of the inputted string, and check each character.
-        for(let i = 0; i < code.length; i++) {
+        for (var i = 0; i < code.length; i++) {
             console.log('LEXER - Lexing line ' + i + 1);
             this.tokenFlag = this.token.GenerateToken(this.tokenStream[i]);
-
-            if(this.tokenFlag) {
+            if (this.tokenFlag) {
                 //Add current token to the token stream.
                 console.log('LEXER - ' + this.token.getTokenCode() + 'Found on line: Position: ');
                 this.tokenStream[i] = code.charAt(i);
                 this.tokenBuffer++;
             }
-
             else {
                 console.log('LEXER ERROR - ' + this.token.getTokenCode() + 'Found on line: Position: ');
             }
         }
-    }
-
-}
+    };
+    return lex;
+}());
+exports.lex = lex;
