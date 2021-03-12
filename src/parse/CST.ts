ec1b5b79b9afe65f2f04116413ca7cd485d52fba
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-03-12 10:28:08 -0500

    created cst class

--- src/parse/CST.ts
+++ src/parse/CST.ts
@@ -1,3 +1,29 @@
+import { treeNode } from 'treeNode';
+
 export class CST {
-    
+    private rootNode : treeNode;
+
+    constructor() {
+        this.rootNode = null;
+    }
+
+    public getRoot() {
+        return this.rootNode;
+    }
+
+    public setRoot(node : treeNode) {
+        this.rootNode = node;
+    }
+
+    //Add nodes to the tree
+    public addNode(nodeVal : number) : boolean {
+        let newNode = treeNode(nodeVal)
+
+        //Check if the node is empty. If it is, make the new node the root node.
+        if(this.rootNode == null) {
+            this.setRoot(newNode);
+            return true;
+        }
+    }
+
 }
