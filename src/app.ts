8d801f5dac64a2410c4cc87532ca99deb6edf108
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-03-12 20:40:04 -0500

    added frontend browser components

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json({ type: 'application/json' }));
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: './src/views'});
+});
+
+app.listen(port, function () {
+    return console.log("server is listening on " + port);
+});
