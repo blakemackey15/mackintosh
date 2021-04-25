14cdb5903f8f8144c1c11f119ce95980ecdbcb96
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-04-25 15:54:50 -0400

    created scope tree node and tree classes

--- dist/semanticAnalysis/scopeTree.js
+++ dist/semanticAnalysis/scopeTree.js
@@ -4,16 +4,44 @@ var mackintosh;
     var scopeTreeNode = /** @class */ (function () {
         function scopeTreeNode(key, values) {
             this.scopeMap = new Map();
-            this.children = [];
+            this.values = values;
             this.scopeMap.set(key, values);
+            this.isUsed = false;
         }
+        scopeTreeNode.prototype.addValue = function (key, value) {
+            this.values.push(value);
+            this.scopeMap.set(key, this.values);
+        };
+        scopeTreeNode.prototype.getValues = function () {
+            return this.scopeMap.values();
+        };
+        scopeTreeNode.prototype.setIsUsed = function (isUsed) {
+            this.isUsed = isUsed;
+        };
+        scopeTreeNode.prototype.getIsUsed = function () {
+            return this.isUsed;
+        };
         return scopeTreeNode;
     }());
     mackintosh.scopeTreeNode = scopeTreeNode;
     //Class to represent the scope tree - tree of hash maps.
     var scopeTree = /** @class */ (function () {
-        function scopeTree() {
+        function scopeTree(parent) {
+            this.children = [];
+            this.parent = parent;
         }
+        scopeTree.prototype.getParentScope = function () {
+            return this.parent;
+        };
+        scopeTree.prototype.createChildScope = function (values, key) {
+            this.children.push(new scopeTreeNode(key, values));
+        };
+        scopeTree.prototype.getChild = function (scopePointer) {
+            return this.children[scopePointer];
+        };
+        //Print out symbol table tree.
+        scopeTree.prototype.toString = function () {
+        };
         return scopeTree;
     }());
     mackintosh.scopeTree = scopeTree;
--- src/semanticAnalysis/scopeTree.ts
+++ src/semanticAnalysis/scopeTree.ts
@@ -1,18 +1,59 @@
 module mackintosh {
     //Class to represent a node in the scope tree.
     export class scopeTreeNode {
-        private parent : scopeTreeNode;
-        private children : Array<scopeTreeNode>;
+        private values : Array<string>;
         private scopeMap = new Map();
+        private isUsed : boolean;
 
         constructor(key : string, values : Array<string>) {
-            this.children = [];
-            this.scopeMap.set(key, values);        
+            this.values = values;
+            this.scopeMap.set(key, values);
+            this.isUsed = false;     
+        }
+
+        public addValue(key : string, value : string) {
+            this.values.push(value);
+            this.scopeMap.set(key, this.values);
+        }
+
+        public getValues() {
+            return this.scopeMap.values();
+        }
+
+        public setIsUsed(isUsed : boolean) {
+            this.isUsed = isUsed;
+        }
+
+        public getIsUsed() : boolean {
+            return this.isUsed;
         }
     }
 
     //Class to represent the scope tree - tree of hash maps.
     export class scopeTree {
-        
+        private children : Array<scopeTreeNode>;
+        private parent : scopeTreeNode;
+
+        constructor(parent : scopeTreeNode) {
+            this.children = [];
+            this.parent = parent;
+        }
+
+        public getParentScope() : scopeTreeNode {
+            return this.parent;
+        }
+
+        public createChildScope(values : Array<string>, key : string) {
+            this.children.push(new scopeTreeNode(key, values));
+        }
+
+        public getChild(scopePointer : number) : scopeTreeNode {
+            return this.children[scopePointer];
+        }
+
+        //Print out symbol table tree.
+        public toString() {
+
+        }
     }
 }
