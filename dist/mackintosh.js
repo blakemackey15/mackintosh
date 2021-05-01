830cb8dfc70c5968b6714d908cad5d063076676c
blakemackey15 <54688846+blakemackey15@users.noreply.github.com>
2021-04-30 23:54:54 -0400

    updated symbol tree, node, and scope classes

--- dist/mackintosh.js
+++ dist/mackintosh.js
@@ -1119,15 +1119,56 @@ var mackintosh;
 (function (mackintosh) {
     //Represents a node in the symbol table.
     var symbolTableNode = /** @class */ (function () {
-        function symbolTableNode() {
+        function symbolTableNode(map) {
+            if (map === void 0) { map = new Map(); }
+            this.hashmap = new Map();
+            this.hashmap = map;
+            this.children = [];
         }
+        symbolTableNode.prototype.setMap = function (map) {
+            if (map === void 0) { map = new Map(); }
+            this.hashmap = map;
+        };
+        symbolTableNode.prototype.getMap = function () {
+            return this.hashmap;
+        };
+        symbolTableNode.prototype.setParentScope = function (parent) {
+            this.parent = parent;
+        };
+        symbolTableNode.prototype.getParentScope = function () {
+            return this.parent;
+        };
+        symbolTableNode.prototype.getChildren = function () {
+            return this.children;
+        };
+        symbolTableNode.prototype.addChild = function (child) {
+            this.children.push(child);
+        };
         return symbolTableNode;
     }());
     mackintosh.symbolTableNode = symbolTableNode;
     //Represent the symbol table tree.
     var symbolTableTree = /** @class */ (function () {
         function symbolTableTree() {
+            this.rootNode = null;
         }
+        symbolTableTree.prototype.getRoot = function () {
+            return this.rootNode;
+        };
+        symbolTableTree.prototype.getCurNode = function () {
+            return this.curNode;
+        };
+        symbolTableTree.prototype.addNode = function (map) {
+            if (map === void 0) { map = new Map(); }
+            var node = new symbolTableNode(map);
+            if (this.rootNode == null) {
+                this.rootNode = node;
+            }
+            else {
+                node.setParentScope(this.curNode);
+                this.curNode.addChild(node);
+            }
+        };
         return symbolTableTree;
     }());
     mackintosh.symbolTableTree = symbolTableTree;
--- src/semanticAnalysis/symbolTableTree.ts
+++ src/semanticAnalysis/symbolTableTree.ts
@@ -1,20 +1,71 @@
 module mackintosh {
     //Represents a node in the symbol table.
     export class symbolTableNode {
+        private hashmap = new Map();
         private children : Array<symbolTableNode>;
         private parent : symbolTableNode;
 
-        constructor() {
-            
+        constructor(map = new Map()) {
+            this.hashmap = map;
+            this.children = [];
+        }
+
+        public setMap(map = new Map()) {
+            this.hashmap = map;
+        }
+
+        public getMap() {
+            return this.hashmap;
+        }
+
+        public setParentScope(parent) {
+            this.parent = parent;
+        }
+
+        public getParentScope() : symbolTableNode {
+            return this.parent;
+        }
+
+        public getChildren() : Array<symbolTableNode> {
+            return this.children;
+        }
+
+        public addChild(child : symbolTableNode) {
+            this.children.push(child);
         }
 
     }
 
     //Represent the symbol table tree.
     export class symbolTableTree {
+        private rootNode : symbolTableNode;
+        private curNode : symbolTableNode;
 
         constructor() {
-
+            this.rootNode = null;
         }
+
+        public getRoot() {
+            return this.rootNode;
+        }
+
+        public getCurNode() {
+            return this.curNode;
+        }
+
+        public addNode(map = new Map()) {
+            let node = new symbolTableNode(map);
+
+            if(this.rootNode == null) {
+                this.rootNode = node;
+            }
+
+            else {
+                node.setParentScope(this.curNode);
+                this.curNode.addChild(node);
+            }
+        }
+
+        
     }
 }
